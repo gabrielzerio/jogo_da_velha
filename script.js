645eee57b4b6c0aeed8d35bd8d17dd99e67d5d5b
Gabriel <21043296+gabrielzerio@users.noreply.github.com>
2023-06-17 13:14:10 -0300

    click on box working

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const form = document.querySelector("#myForm");
 
-function getPlayersNames(e){
+function getPlayersNames(e) {
     //previnir o envio do formulario
     e.preventDefault();
     const formData = new FormData(form);
@@ -11,17 +11,40 @@ function getPlayersNames(e){
 }
 form.addEventListener('submit', getPlayersNames);
 
-const initilizeVariables = (data) =>{
+const initilizeVariables = (data) => {
     data.choice = +data.choice;
-    data.board = [0,1,2,3,4,5,6,7,8];
+    data.board = [0, 1, 2, 3, 4, 5, 6, 7, 8];
     data.player1 = 'X';
     data.player2 = 'O';
     data.round = 0;
-    data.currentPlayer = 'X';
+    data.currentPlayer = 'O';
     data.gameOver = false;
 }
 
-const initilizeGame = (data) =>{
+const gameBoardEventListeners = (data) => {
+    document.querySelectorAll(".box").forEach(box => {
+        box.addEventListener("click", (e) => {
+            playMove(e.target, data);
+        });
+    });
+};
+
+const initilizeGame = (data) => {
     initilizeVariables(data);
-    console.log(data);
+    gameBoardEventListeners(data);
+}
+
+const playMove = (box, data) => {
+    if (data.gameOver || data.round > 8) { //se a variavel gameOver for true o jogo não continua
+        return;
+    }
+    if (data.board[box.id] === "X" || data.board[box.id] === "O") { //se o array não tiver um X ou O no vetor indicado pelo box.id
+       return;
+    }
+    data.board[box.id] = data.currentPlayer;
+    box.textContent = data.currentPlayer;
+    box.dataset.player = data.currentPlayer === "X" ? "player1" : "player2";
+    data.round++;
+    // console.log(box,data);
+    
 }
