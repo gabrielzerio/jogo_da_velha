fed5e230f27ab9a4389e6ecd4dba104e263df009
Gabriel <21043296+gabrielzerio@users.noreply.github.com>
2023-06-17 12:25:49 -0300

    game variables created

--- script.js
+++ script.js
@@ -11,3 +11,17 @@ function getPlayersNames(e){
 }
 form.addEventListener('submit', getPlayersNames);
 
+const initilizeVariables = (data) =>{
+    data.choice = +data.choice;
+    data.board = [0,1,2,3,4,5,6,7,8];
+    data.player1 = 'X';
+    data.player2 = 'O';
+    data.round = 0;
+    data.currentPlayer = 'X';
+    data.gameOver = false;
+}
+
+const initilizeGame = (data) =>{
+    initilizeVariables(data);
+    console.log(data);
+}
